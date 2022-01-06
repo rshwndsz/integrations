93e0a8970ed1d294ac1d17e789dd5ca067b9562d
rshwndsz <30742639+rshwndsz@users.noreply.github.com>
2022-01-06 16:13:40 +0530

    Replace prints with logging

--- goodreads.py
+++ goodreads.py
@@ -1,4 +1,6 @@
 import argparse
+import logging
+
 import requests
 from tqdm import tqdm
 from bs4 import BeautifulSoup
@@ -31,17 +33,15 @@ def scrape(id, driver):
     driver.get(url)
 
     wait = WebDriverWait(driver, timeout=10)
-    retries = 1 
-    while retries <= 3:
+    for retry in range(3):
         try:
             wait.until(EC.presence_of_element_located((By.ID, "coverImage")))
             soup = BeautifulSoup(driver.page_source, "html.parser")
             return soup
         except TimeoutException:
+            logging.info(f"TimeoutException while trying {url}. Trying again...")
             driver.refresh()
-            retries += 1
 
-    # Could not load soup
     return None
 
 
@@ -51,6 +51,7 @@ def update_export(args):
 
     # Debug mode: Use just 6 rows
     if args.debug:
+        logging.info("Using only 6 rows in 'debug' mode.")
         df = df[:6]
 
     # Create new columns for Genre & Cover Image
@@ -60,6 +61,7 @@ def update_export(args):
     # Setup Firefox Driver
     options = webdriver.FirefoxOptions()
     if args.headless:
+        logging.info("Starting Firefox driver in 'headless' mode.")
         options.add_argument("--headless")
     options.set_capability("pageLoadStrategy", "eager")
     driver = webdriver.Firefox(options=options)
@@ -68,13 +70,15 @@ def update_export(args):
     for index, row in tqdm(df.iterrows(), total=len(df)):
         soup = scrape(str(row["Book Id"]), driver)
         if soup is None:
-            print(f"Could not update {row['Book Id']}: {row['Title']}")
+            logging.warning(f"Could not update {row['Book Id']}: {row['Title']}. Skipping...")
             continue
 
         genres = get_genres(soup)
         df["Genres"] = ", ".join(genres)
 
         image = get_coverimage(soup)
+        if image is None:
+            logging.warning(f"Could not get cover image for {row['Book Id']}: {row['title']}.")
         df["Cover Image"] = image if image is not None else ""
 
     driver.quit()
@@ -87,7 +91,14 @@ if __name__ == "__main__":
     parser.add_argument("--debug", dest="debug", action="store_true")
     parser.add_argument("--input", default="./input.csv")
     parser.add_argument("--output", default="./output.csv")
-
     args = parser.parse_args()
+
+    # Config for the Root Logger
+    logging.basicConfig(
+        format="[%(levelname)s] :: %(asctime)s :: %(message)s in %(pathname)s at (lineno)d",
+        filename="LOG.log",
+        filemode="a",
+        level=logging.DEBUG if args.debug else logging.INFO,
+    )
 
     update_export(args)
